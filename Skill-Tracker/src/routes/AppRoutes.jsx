c911030649ecb70500d347cefa6fda3f5a60d178
Adil ali <adil.ali_cs22@gla.ac.in>
2026-08-09 10:29:36 +0530

    adding app routes

--- Skill-Tracker/src/routes/AppRoutes.jsx
+++ Skill-Tracker/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<h1 className="text-black p-10">Home Page</h1>} />
-            <Route path="/login" element={<h1 className="text-black p-10">Login Page</h1>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/dashboard" element={<h1 className="text-black p-10">Dashboard Page</h1>} />
         </Routes>
     )
