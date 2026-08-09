4ebb484c499240b6e4aec8163493bb0addefb09c
Adil ali <adil.ali_cs22@gla.ac.in>
2026-08-09 10:34:03 +0530

    Update app route

--- Skill-Tracker/src/routes/AppRoutes.jsx
+++ Skill-Tracker/src/routes/AppRoutes.jsx
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/dashboard" element={<h1 className="text-black p-10">Dashboard Page</h1>} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <h1 className="text-black p-10">Dashboard Page</h1>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
